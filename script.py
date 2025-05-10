9204d78f18a1944f37dc080b2bf7d6365110e88a
João Eduardo <joaoeduardodev2212@gmail.com>
2025-05-10 00:27:51 -0300

    Corrected some centering issues

--- script.py
+++ script.py
@@ -1,24 +1,27 @@
 import tkinter as tk
 import pygetwindow as gw
 import pyautogui
+import logging
 from pynput import keyboard
 
-# Global Variables
+logging.basicConfig(level=loging.INFO)
+logging.info("No active window found!")
+
 keypresscount = 0
-window = gw.getActiveWindow()
 screen_width, screen_height= pyautogui.size()
-window_width = window.width
-window_height = window.height
-x = (screen_width - window_width) // 2
-y = (screen_height - window_height) // 2
 
 def on_press(key):
     global keypresscount
     if key == keyboard.Key.shift_r:
         keypresscount += 1
         if keypresscount == 3:
+            window = gw.getActiveWindow()
             if window:
-                window.moveTo(x, y)
+                window_width = window.width
+                window_height = window.height
+                width = (screen_width - window_width) // 2
+                height = (screen_height - window_height) // 2
+                window.moveTo(width, height)
             else:
                 print("No active window found!")
             keypresscount = 0
