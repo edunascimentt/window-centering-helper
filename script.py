d19aa79647c5ca06f011df6ca3645258e5f067bc
João Eduardo <joaoeduardodev2212@gmail.com>
2025-05-11 02:15:23 -0300

    add button for ON/OFF and add some gui

--- script.py
+++ script.py
@@ -1,17 +1,31 @@
+# external packages
 import tkinter as tk
+from tkinter import IntVar
 import pygetwindow as gw
 import pyautogui
-import logging
 from pynput import keyboard
+import threading
 
-logging.basicConfig(level=loging.INFO)
-logging.info("No active window found!")
-
+#global variables
 keypresscount = 0
 screen_width, screen_height= pyautogui.size()
 
+# gui
+guiwindow = tk.Tk()
+
+guiwindow.geometry("350x500")
+guiwindow.title("Window centering helper")
+guiwindow.resizable(False, False)
+
+getvalue = IntVar()
+on_off = tk.Checkbutton(guiwindow, text="Toggle centering on", font=50, variable=getvalue, onvalue=1, offvalue=0)
+on_off.pack()
+
 def on_press(key):
     global keypresscount
+
+    if (getvalue.get()==0):
+        return
     if key == keyboard.Key.shift_r:
         keypresscount += 1
         if keypresscount == 3:
@@ -26,5 +40,18 @@ def on_press(key):
                 print("No active window found!")
             keypresscount = 0
 
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+
+def start_listener():
+    def run_listener():
+        with keyboard.Listener(on_press=on_press) as listener:
+            listener.join()
+
+    # Run the listener in a separate thread
+    listener_thread = threading.Thread(target=run_listener, daemon=True)
+    listener_thread.start()
+
+# Start the keyboard listener
+start_listener()
+
+# Keeping program running
+guiwindow.mainloop()
